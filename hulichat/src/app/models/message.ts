495331357d3ddea17e288c85e6daf52ddf746c08
Attila Korom <attilakorom2014@gmail.com>
2018-01-10 15:02:57 +0100

    feat(components): create components

---
+++ hulichat/src/app/models/message.ts
@@ -0,0 +1,7 @@
+export class Message {
+    message_id : Number
+    from_user_id : Number
+    to_user_id : Number
+    text_message : String
+    time_stamp : String
+}
---
+++ hulichat/src/app/models/user.ts
@@ -0,0 +1,6 @@
+export class User {
+    user_id : Number
+    user_name : String
+    user_pw : String
+    user_pic ?: String
+}
