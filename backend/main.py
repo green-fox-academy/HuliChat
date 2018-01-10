ca463ad52b6d0b59f32233ca1cc602a2812d9dd5
peter-keller <keller.f.peter@gmail.com>
2018-01-10 13:15:12 +0100

    feat(sql): connection created, sql file too

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flaskext.mysql import MySQL
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 mysql = MySQL()
@@ -7,10 +7,13 @@ mysql = MySQL()
 
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-app.config['MYSQL_DATABASE_DB'] = 'chat'
+app.config['MYSQL_DATABASE_DB'] = 'user_info'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
+
+conn = mysql.connect()
+cursor = conn.cursor()
 
 @app.route('/')
 def index():
