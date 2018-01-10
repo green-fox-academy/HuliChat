eb2b7fc31e5b110fa6f9ef61da59217c69cdd62c
peter-keller <keller.f.peter@gmail.com>
2018-01-10 15:48:33 +0100

    feat(main): backend working version

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,81 @@
-from flask import Flask
-from flask_mysqldb import MySQL
+#from flask import Flask
+#import MySQLdb
+#from flaskext.mysql import MySQL
 
+#app = Flask(__name__)
+#mysql = MySQL()
+
+from flask import Flask, request, render_template
+import pymysql
 app = Flask(__name__)
-mysql = MySQL()
+db = pymysql.connect("localhost", "root", "root", "chat")
 
 
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-app.config['MYSQL_DATABASE_DB'] = 'user_info'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
 
 
-conn = mysql.connect()
-cursor = conn.cursor()
+@app.route('/cars')
+def someName():
+    cursor = db.cursor()
+    sql = "SELECT * FROM licence_plates"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    return render_template('index.html', results=results)
 
-@app.route('/')
-def index():
-    return "Homepage"
+#app.config['MYSQL_DATABASE_USER'] = 'root'
+#app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+#app.config['MYSQL_DATABASE_DB'] = 'chat'
+#app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+#mysql.init_app(app)
+#conn = MySQLdb.connect(host="localhost",
+#                         user="root",
+#                         passwd="root",
+#                         db="chat")
+#conn = mysql.connect()
+#cursor = conn.cursor()
+#@app.route('/')
+#def index():
+
+
+       #cursor.execute('SELECT * FROM licence_plates')
+    #data = cursor.fetchall()
+    #return data
+    
+    #c = conn.cursor()
+    #asd = c.execute('''SELECT * FROM licence_plates''')
+    #return asd.fetchall()
+
+    #return c.fetchall()
+    #return c, conn
+    #cur = mysql.connection.cursor()
+    #cur.execute('SHOW TABLES;')
+    #rv = cur.fetchall()
+    #return str(rv)
+
+
+@app.route('/signup')
+def signup():
+    return "register"
+
+
+@app.route('/login')
+def login():
+    return "login"
+
+
+@app.route('/messages')
+def messages():
+    return "messages"
+
+
+@app.route('/users')
+def users():
+    return "users"
+
+#signup
+#login
+#messages
+#users
+#
 
 
 if __name__ == "__main__":
