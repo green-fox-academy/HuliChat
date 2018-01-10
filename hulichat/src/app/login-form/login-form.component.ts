4c5047d4b8108384831cc8baebd0a3d44e4f50e2
Attila Korom <attilakorom2014@gmail.com>
2018-01-10 19:03:03 +0100

    feat(authService): add authtentication service

--- hulichat/src/app/login-form/login-form.component.ts
+++ hulichat/src/app/login-form/login-form.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css']
 })
-export class LoginFormComponent implements OnInit {
+export class LoginFormComponent {
 
-  constructor() { }
+  email : String
+  password : String
+  errorMessage : String
 
-  ngOnInit() {
+  constructor(private router : Router, private authService : AuthenticationService) { }
+
+  login() {
+    this.authService.login(this.email, this.password).catch(error => this.errorMessage = error.message);
   }
 
 }
