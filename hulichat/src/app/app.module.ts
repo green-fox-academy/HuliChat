71f64b598f173377451fdb847c7a73be56a9de68
Attila Korom <attilakorom2014@gmail.com>
2018-01-10 15:42:15 +0100

    feat(html_skeletons): create skeleton of htmls

--- hulichat/src/app/app.module.ts
+++ hulichat/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { HeaderComponent } from './header/header.component';
 import { UsersFormComponent } from './users-form/users-form.component';
 import { ChatFormComponent } from './chat-form/chat-form.component';
 import { ChatWindowComponent } from './chat-window/chat-window.component';
+import { UserItemComponent } from './user-item/user-item.component';
+import { MessageItemComponent } from './message-item/message-item.component';
 
 
 @NgModule({
@@ -19,7 +21,9 @@ import { ChatWindowComponent } from './chat-window/chat-window.component';
     HeaderComponent,
     UsersFormComponent,
     ChatFormComponent,
-    ChatWindowComponent
+    ChatWindowComponent,
+    UserItemComponent,
+    MessageItemComponent
   ],
   imports: [
     BrowserModule,
