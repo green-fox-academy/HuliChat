481d2da7fb8efbabb4dff23927fdb8c03a46fbb3
peter-keller <keller.f.peter@gmail.com>
2018-01-10 17:17:42 +0100

    feat(main): returns json

--- backend/main.py
+++ backend/main.py
@@ -1,55 +1,29 @@
-#from flask import Flask
-#import MySQLdb
-#from flaskext.mysql import MySQL
+from flask import Flask, jsonify,json
+from flaskext.mysql import MySQL
 
-#app = Flask(__name__)
-#mysql = MySQL()
 
-from flask import Flask, json, request, render_template
-import pymysql
 app = Flask(__name__)
-db = pymysql.connect("localhost", "root", "root", "chat")
+mysql = MySQL()
+
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'chat'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+mysql.init_app(app)
 
 
 
+@app.route('/')
+def get():
+    cur = mysql.connect().cursor()
+    cur.execute('''select * from chat.licence_plates''')
+    r = [dict((cur.description[i][0], value)
+              for i, value in enumerate(row)) for row in cur.fetchall()]
+    return json.dumps({'myCollection' : r})
 
-@app.route('/cars')
-def someName():
-    cursor = db.cursor()
-    sql = "SELECT * FROM licence_plates"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    return json.dumps(results)
-
-#app.config['MYSQL_DATABASE_USER'] = 'root'
-#app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-#app.config['MYSQL_DATABASE_DB'] = 'chat'
-#app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-#mysql.init_app(app)
-#conn = MySQLdb.connect(host="localhost",
-#                         user="root",
-#                         passwd="root",
-#                         db="chat")
-#conn = mysql.connect()
-#cursor = conn.cursor()
-#@app.route('/')
-#def index():
-
-
-       #cursor.execute('SELECT * FROM licence_plates')
-    #data = cursor.fetchall()
-    #return data
-    
-    #c = conn.cursor()
-    #asd = c.execute('''SELECT * FROM licence_plates''')
-    #return asd.fetchall()
-
-    #return c.fetchall()
-    #return c, conn
-    #cur = mysql.connection.cursor()
-    #cur.execute('SHOW TABLES;')
-    #rv = cur.fetchall()
-    #return str(rv)
 
 
 @app.route('/signup')
