27f72f877d278a4ca15e52fef2c3a7d09b79fc97
peter-keller <keller.f.peter@gmail.com>
2018-01-11 00:24:09 +0100

    feat(db): refreshed test db

--- backend/database.sql
+++ backend/database.sql
@@ -1,12 +1,12 @@
 CREATE TABLE message_data ( message_text TEXT , from_user_id INT(50) , to_user_id INT(50) , time_stamp TIMESTAMP(6) , message_id INT(50));
-
 CREATE TABLE user_data ( user_id INT(50) , user_pw VARCHAR(50) , user_pic TEXT , user_name VARCHAR(50));
 
 
 
 
-INSERT INTO message_data (message_text, from_user_id, to_user_id, time_stamp, message_id) values ('SZIA HOGYVAGY?', 0, 1, '', 0);
-INSERT INTO user_data (user_id, user_pw, user_pic, user_name) values (0, "asd123", "something", "testytest")
-
-
-INSERT INTO message_data (message_text, from_user_id, to_user_id, time_stamp, message_id) values ('pleasework', 0, 1, NOW, 2);
+INSERT INTO message_data (message_text, from_user_id, to_user_id, time_stamp, message_id) values ('SZIA HOGYVAGY?', 0, 1, NOW(), 0);
+INSERT INTO user_data (user_id, user_pw, user_pic, user_name) values (0, "asd123", "something", "testytest");
+INSERT INTO message_data (message_text, from_user_id, to_user_id, time_stamp, message_id) values ('pleasework', 1, 2, NOW(), 1);
+INSERT INTO user_data (user_id, user_pw, user_pic, user_name) values (1, "not asd123", "somethingelse", "testingstuff");
+INSERT INTO message_data (message_text, from_user_id, to_user_id, time_stamp, message_id) values ('it works guys', 2, 0, NOW(), 2);
+INSERT INTO user_data (user_id, user_pw, user_pic, user_name) values (2, "maybe asd123", "not something else", "testingstuffagain");
